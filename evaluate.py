a1d8a88a158353f7fb7d42e7473be7388187ac4f
Salma0104 <Salmahassan0104@gmail.com>
2024-04-23 01:57:03 +0100

    evaluation script

--- evaluate.py
+++ evaluate.py
@@ -3,19 +3,26 @@ import evaluate
 from nltk.translate import meteor_score
 from datasets import load_dataset
 import numpy as np
+from moverscore_v2 import get_idf_dict, word_mover_score
 
 def evaluate_preds(predictions,references):
   # compute sacrebleu, meteor + mover score
   met_score = [meteor_score.single_meteor_score(p,r,alpha=0.9, beta=3, gamma=0.5) for p,r in zip(predictions,references)]
-  scb_score = scb.compute(predictions = predictions,references = references)
-  
+  scb_score = scb.compute(predictions = predictions,references = [[ref] for ref in references])
+
+  idf_dict_pred = get_idf_dict(predictions)
+  idf_dict_ref = get_idf_dict(references)
+  wm_score = word_mover_score(references, predictions, idf_dict_ref, idf_dict_pred, \
+                          stop_words=[], n_gram=1, remove_subwords=True, batch_size=64)
+  return met_score,scb_score,wm_score
+
 
 
 scb = evaluate.load("sacrebleu")
 data_files = {"test":"test.csv"}
 test_data = load_dataset(path='./processed_data', data_files=data_files)["test"]
-references = [[ref] for ref in test_data["target"]]
 
 for file in os.listdir("./inference_results"):
   predictions = np.loadtxt(f'./training-data/{file}', dtype=str)
-  evaluate_preds(predictions,references)
+  ms,scbs,wms = evaluate_preds(predictions,test_data["target"])
+  print(f"{file}: Meteor: {ms}, SacreBleu: {scbs}, Word Mover Score: {wms}")
