b891067bad5fbcb7923d114dfc97e4e8380ad209
acb19sh <acb19sh@login2.pri.stanage.alces.network>
2024-04-23 10:55:04 +0100

    working eval

--- evaluate_models.py
+++ evaluate_models.py
@@ -12,22 +12,20 @@ transformers.set_seed(2002)
 parser = argparse.ArgumentParser()
 parser.add_argument('--access_token',type=str)
 args = parser.parse_args()
-#from moverscore_v2 import get_idf_dict, word_mover_score
+from moverscore_v2 import get_idf_dict, word_mover_score
 
 def evaluate_preds(predictions,references,checkpoint):
-  # compute sacrebleu, meteor + mover score
+  #compute sacrebleu, meteor + mover score
   tokenizer = AutoTokenizer.from_pretrained(checkpoint,token=args.access_token) 
-  tokenized_refs = [tokenizer.tokenize(ref) for ref in references]
-  tokenized_preds = [tokenizer.tokenize(pred) for pred in predictions]
+  tokenized_refs = [tokenizer.tokenize(ref,max_length=384,truncation=True) for ref in references]
+  tokenized_preds = [tokenizer.tokenize(pred, max_length=384,truncation=True) for pred in predictions]
   met_score = [meteor_score.single_meteor_score(p,r,alpha=0.9, beta=3, gamma=0.5) for p,r in zip(tokenized_preds,tokenized_refs)]
   scb_score = scb.compute(predictions = predictions,references = [[ref] for ref in references])
-
-  # idf_dict_pred = get_idf_dict(predictions)
-  # idf_dict_ref = get_idf_dict(references)
-  wm_score = 0
-  # wm_score = word_mover_score(references, predictions, idf_dict_ref, idf_dict_pred, \
-  #                         stop_words=[], n_gram=1, remove_subwords=True, batch_size=64)
-  return met_score,scb_score,wm_score
+  idf_dict_pred = get_idf_dict(predictions, nthreads=1)
+  idf_dict_ref = get_idf_dict(references,nthreads=1)
+  wm_score = word_mover_score(references, predictions, idf_dict_ref, idf_dict_pred, \
+                           stop_words=[], n_gram=1, remove_subwords=True, batch_size=64)
+  return met_score,scb_score,np.mean(wm_score),np.median(wm_score)
 
 
 
@@ -41,13 +39,8 @@ for file in os.listdir("./inference_results"):
     checkpoint = f"google/{checkpoint}"
   else:
     checkpoint = f"Rocketknight1/{checkpoint}"
-
-  print(checkpoint)
-  #if checkpoint == "google/gemma-2b" or checkpoint == "Rocketknight1/falcon-rw-1b":
-   # continue
   with open(f'./inference_results/{file}','r') as f:
     lines = f.readlines()
   predictions = [line.strip() for line in lines]
-  #print(predictions[-1])
-  ms,scbs,wms = evaluate_preds(predictions,test_data["target"],checkpoint)
-  print(f"{file}: Meteor: {np.mean(ms):.3f}, SacreBleu: {scbs['score']:.3f}, Word Mover Score: {wms:.3f}")
+  ms,scbs,mn_wms,me_wms = evaluate_preds(predictions,test_data["target"],checkpoint)
+  print(f"{file}: Meteor: {np.mean(ms):.3f}, SacreBleu: {scbs['score']:.3f}, Mean Word Mover Score: {mn_wms:.3f}, Median Word Mover Score: {me_wms:.3f}")
